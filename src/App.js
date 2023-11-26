48c769120403789920c9cfdf3d9b4a3bbcc30c11
Sintu Kumar <kumarsintu499@gmail.com>
2023-11-26 22:23:58 +0530

    ,

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Tic_tac_toe from './Components/Tic_tac_toe';
+
+function App() {
+  return (
+    <div>
+      <Tic_tac_toe/>
+    </div>
+  );
+}
+
+export default App;
